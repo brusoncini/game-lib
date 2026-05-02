0377175432af92c76c1ed6a2eb328d037437a635
Bruna Soncini <soncinibruna@gmail.com>
2026-05-02 16:18:05 -0300

    feat: padroniza as respostas de erro da api

--- src/main/java/com/bruna/gamelib/exception/TratadorDeErros.java
+++ src/main/java/com/bruna/gamelib/exception/TratadorDeErros.java
@@ -1,14 +1,50 @@
 package com.bruna.gamelib.exception;
 
+import com.bruna.gamelib.dto.RespostaErro;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @RestControllerAdvice
 public class TratadorDeErros {
 
     @ExceptionHandler(JogoJaImportadoException.class)
-    public ResponseEntity<String> tratarJogoJaImportado(JogoJaImportadoException erro) {
-        return ResponseEntity.status(409).body(erro.getMessage());
+    public ResponseEntity<RespostaErro> tratarJogoJaImportado(JogoJaImportadoException erro) {
+        RespostaErro resposta = new RespostaErro(
+                409,
+                "Conflito",
+                erro.getMessage()
+        );
+
+        return ResponseEntity.status(409).body(resposta);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<RespostaErro> tratarParametroInvalido(MethodArgumentTypeMismatchException erro) {
+        String mensagem = "Algum parametro enviado esta em formato invalido.";
+
+        if ("status".equals(erro.getName())) {
+            mensagem = "Status invalido. Use: QUERO_JOGAR, JOGANDO, JOGADO, ZERADO ou ABANDONADO.";
+        }
+
+        RespostaErro resposta = new RespostaErro(
+                400,
+                "Parametro invalido",
+                mensagem
+        );
+
+        return ResponseEntity.badRequest().body(resposta);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<RespostaErro> tratarErroGenerico(Exception erro) {
+        RespostaErro resposta = new RespostaErro(
+                500,
+                "Erro interno",
+                "Ocorreu um erro inesperado no servidor."
+        );
+
+        return ResponseEntity.status(500).body(resposta);
     }
 }
